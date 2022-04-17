067804e77971c4d367a95e7248ed4584995da2cb
hjkim <haejin475@gmail.com>
2022-04-17 13:18:41 +0900

    [firebase] 전역변수로 빼기

--- db/firebase.go
+++ db/firebase.go
@@ -2,27 +2,26 @@ package db
 
 import (
 	"context"
-	"fmt"
-	"log"
-
 	firebase "firebase.google.com/go"
+	"fmt"
 	"google.golang.org/api/option"
+	"log"
 )
 
-func Firebase() {
-	// SDK key
-	opt := option.WithCredentialsFile("db/key.json")
+var ctx = context.Background()
+var opt = option.WithCredentialsFile("db/key.json") // SDK key
 
+func Firebase() {
 	config := &firebase.Config{ProjectID: "<FIREBASE_PROJECT_ID>"}
 	println("[HJLOG] config : ", config)
 
-	var app, err = firebase.NewApp(context.Background(), nil, opt)
+	var app, err = firebase.NewApp(ctx, nil, opt)
 	fmt.Printf("[HJLOG] app : %s , err : %s\n", app, err)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	client, err := app.Auth(context.Background())
+	client, err := app.Auth(ctx)
 	fmt.Printf("[HJLOG] client : %s , err : %s\n", client, err)
 	if err != nil {
 		log.Fatalf("error getting Auth client: %v\n", err)
